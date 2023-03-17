75cf8d29b225b27b5b99e050c1d5d6cb64ed77e6
Phoenix Tsui <xusiyuan841028@gmail.com>
2023-03-18 00:03:54 +0800

    fix: use https git clone

--- zsh/custom/zshrc.zsh
+++ zsh/custom/zshrc.zsh
@@ -37,14 +37,13 @@ try_source $DOTFILES/zsh/custom/config/tools/fzf/*.zsh
 local custom_path=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}
 if [[ ! -d $custom_path/plugins/fzf-git.sh ]] {
   echo "$fg[blue]Install Plugin: $fg_bold[green]junegunn/fzf-git$reset_color"
-  git clone git@github.com:junegunn/fzf-git.sh.git $custom_path/plugins/fzf-git.sh
-  git clone git@github.com:junegunn/fzf-git.sh.git $custom_path/plugins/fzf-git.sh
+  git clone https://github.com/junegunn/fzf-git.sh.git $custom_path/plugins/fzf-git.sh
 }
 source $custom_path/plugins/fzf-git.sh/fzf-git.sh
 
 if [[ ! -d $custom_path/plugins/zsh-completions ]] {
   echo "$fg[blue]Install Plugin: $fg_bold[green]zsh-users/zsh-completions$reset_color"
-  git clone git@github.com:zsh-users/zsh-completions $custom_path/plugins/zsh-completions
+  git clone https://github.com/zsh-users/zsh-completions $custom_path/plugins/zsh-completions
 }
 fpath=("$fpath[@]" $custom_path/plugins/zsh-completions/src)
 

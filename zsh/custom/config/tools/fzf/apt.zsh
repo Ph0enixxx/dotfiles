6cc0fcef829787546e7d5325b098538c804a6b78
Phoenix Tsui <xusiyuan841028@gmail.com>
2023-03-20 22:35:59 +0800

    fix: use apt-cache asap.

--- zsh/custom/config/tools/fzf/apt.zsh
+++ zsh/custom/config/tools/fzf/apt.zsh
@@ -6,7 +6,7 @@ APT_FORMAT="awk -F\"/\" '{ if (NR > 1) print \$1 }'"
 
 # [A]pt [I]nstall
 function zai() {
-  local packages=$(apt list | eval $APT_FORMAT | fzf ${=FZF_FULLSCREEN_OPTIONS} -m --query="$1" --preview 'apt list -a {} && apt show -a {}')
+  local packages=$(apt list | eval $APT_FORMAT | fzf ${=FZF_FULLSCREEN_OPTIONS} -m --query="$1" --preview 'apt list -a {} && apt-cache show {}')
 
   if [[ $packages ]]; then
     local packagesStr=$(echo $packages | eval $AWK_JOIN)
@@ -18,7 +18,7 @@ function zai() {
 
 # [A]pt [R]emove
 function zar() {
-  local packages=$(apt list --installed | eval $APT_FORMAT | fzf ${=FZF_FULLSCREEN_OPTIONS} -m --query="$1" --preview 'apt list -a {} && apt show -a {}')
+  local packages=$(apt list --installed | eval $APT_FORMAT | fzf ${=FZF_FULLSCREEN_OPTIONS} -m --query="$1" --preview 'apt list -a {} && apt-cache show {}')
   if [[ $packages ]]; then
     local packagesStr=$(echo $packages | eval $AWK_JOIN)
     echo "Remove: $packagesStr"

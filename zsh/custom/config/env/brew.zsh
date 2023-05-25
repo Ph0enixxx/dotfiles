f7f70d187bb69e66dfb723240a25ab67e453e848
Phoenix Tsui <xusiyuan841028@gmail.com>
2023-05-25 22:35:36 +0800

    feat: use $MIRROR as mirror switch

--- zsh/custom/config/env/brew.zsh
+++ zsh/custom/config/env/brew.zsh
@@ -2,8 +2,21 @@ if ! command -v brew &> /dev/null; then
   return
 fi
 
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
-export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+if [[ $USE_MIRROR == 1 ]] {
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+  export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+}
 
+if [[ $System == 'Darwin' ]] {
+  if [[ -z $(id $USER | grep '(admin)') ]] {
+    export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+  }
+}
+
+if { command -v bat &> /dev/null } {
+  export HOMEBREW_BAT=1
+}
+
+export HOMEBREW_COLOR=1
--- zsh/custom/config/env/nvm.zsh
+++ zsh/custom/config/env/nvm.zsh
@@ -1,2 +1,4 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
+if [[ $USE_MIRROR == 1 ]] {
+  export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
+}

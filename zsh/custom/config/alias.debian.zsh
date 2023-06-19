3741dc9add8c46353811d408690443b287f6aed4
Phoenix Tsui <xusiyuan841028@gmail.com>
2023-06-20 00:19:25 +0800

    feat: 20230620

--- zsh/custom/config/alias.debian.zsh
+++ zsh/custom/config/alias.debian.zsh
@@ -10,3 +10,11 @@ fi
 alias fd='fdfind'
 
 alias test-hd='sudo hpparm -tT --direct'
+
+if command -v docker &>/dev/null; then
+  alias docker='sudo docker'
+fi
+
+if command -v podman &>/dev/null; then
+  alias podman='sudo podman'
+fi
--- zsh/custom/config/tools/fzf/docker.zsh
+++ zsh/custom/config/tools/fzf/docker.zsh
@@ -5,41 +5,48 @@ fi
 
 AWK_DOCKER_FORMAT="awk '{ if (NR > 1) print \$1\":\"\$2 }'"
 
+function __remove_docker_image() {
+  read -q "choice?Remove image: {}? [y/N]"
+  if [[ $choice == 'y' ]]; then
+    docker image rm $1
+  fi
+}
+
+
 # [D]ocker [I]mage
 function zdi() {
-  sudo -b docker version &> /dev/null
+  if [[ $System != 'Darwin' ]]; then
+    sudo -b docker version &> /dev/null
+  fi
   local image='-'
   while [[ -n $image ]] {
-    image=$(sudo docker image ls | fzf +m --height=100%  --nth=1,2,3 --query="$1" --header-lines=1 --preview 'sudo docker image inspect {3} | highlight -O ansi -s solarized-dark -S json' \
+    image=$(docker image ls | fzf +m --height=100%  --nth=1,2,3 --query="$1" --header-lines=1 --preview 'docker image inspect {3} | highlight -O ansi -s solarized-dark -S json' \
       --color='header:italic:underline,label:blue' \
       --header $'CTRL-O (open in browser) / ALT-D (delete)\n\n' \
       --bind "ctrl-o:execute-silent:open https://hub.docker.com/_/{1}" \
       --bind "alt-d:execute:__remove_docker_image {3}" \
-      --bind "alt-o:execute:echo __remove_docker_image" \
+      --bind "alt-o:execute:where __remove_docker_image" \
       --border-label '   Docker Image List ') || return
     image=$(awk '{print $3}'<<<$image)
     echo $image
   }
 }
 
-function __remove_docker_image() {
-  read -q "choice?Remove image: {}? [y/N]"
-  if [[ $choice == 'y' ]]; then
-    sudo docker image rm $1
-  fi
-}
-
 # [D]ocker [C]ontainer
 function zdc() {
-  sudo -b docker version &> /dev/null
-  local container=$(sudo docker container ls |fzf +m --height=100% --header-lines=1 \
-    --query="$1" --preview-window right:50% --preview 'sudo docker container inspect {1} --size | highlight -O ansi -s solarized-dark -S json')
+  if [[ $System != 'Darwin' ]]; then
+    sudo -b docker version &> /dev/null
+  fi
+  local container=$(docker container ls |fzf +m --height=100% --header-lines=1 \
+    --query="$1" --preview-window right:50% --preview 'docker container inspect {1} --size | highlight -O ansi -s solarized-dark -S json')
   echo $container
 }
 
 # [D]ocker [S]earch
 function zds() {
-  sudo -b docker version &> /dev/null
+  if [[ $System != 'Darwin' ]]; then
+    sudo -b docker version &> /dev/null
+  fi
   local input=$1
   if [[ ! $input ]] {
     echo "$fg_bold[green]Please input the search term:$reset_color"
@@ -47,14 +54,14 @@ function zds() {
   }
   local image='-'
   while [[ -n $image ]] {
-    image=$(sudo docker search --no-trunc --limit=100 $input | fzf +m --no-preview --height=100% --nth=1,2 --header-lines=1 \
+    image=$(docker search --no-trunc --limit=100 $input | fzf +m --no-preview --height=100% --nth=1,2 --header-lines=1 \
       --header $'Enter (pull image) ╱ CTRL-O (open in browser)\n\n' \
       --color='header:italic:underline,label:blue' \
       --bind "ctrl-o:execute-silent:if [[ {1} =~ '/' ]]; then open https://hub.docker.com/r/{1}; else open https://hub.docker.com/_/{1}; fi;" \
       --border-label '   Docker Image Search ') || return
     image=$(awk '{print $1}'<<<$image)
     echo "$fg_bold[green]docker pull $fg_bold[blue]$image$reset_color"
-    sudo docker pull $image
+    docker pull $image
   }
 }
 

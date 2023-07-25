9f90aa345450f2b546eedde39e81232f03a00d31
Phoenix Tsui <xusiyuan841028@gmail.com>
2023-07-25 10:38:23 +0800

    feat(tmux): no-tmux profile

--- zsh/custom/config/tools/tmux.zsh
+++ zsh/custom/config/tools/tmux.zsh
@@ -5,6 +5,10 @@ if (( $+TMUX )) || (( $+SSH_CLIENT )) || (( $+TMUX_DISABLE )); then
   return
 fi
 
+if [[ $ITERM_PROFILE == "no-tmux" ]]; then
+  return
+fi
+
 local unattached_session=$(tmux list-sessions | awk -F: '{ if($0 !~ /\(attached\)$/) print $1; }' | head -n 1)
 if [[ "$unattached_session" != "" ]] ; then
   debug 'tmux' 'try attach session: $unattached_session'
@@ -12,3 +16,4 @@ if [[ "$unattached_session" != "" ]] ; then
 else
   tmux
 fi
+
